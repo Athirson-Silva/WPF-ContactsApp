ed793c9e642aafb7f620df6db672d3a67e02661b
Athirson Silva <athirson07@gmail.com>
2023-11-08 01:44:02 -0300

    first commit

---
+++ ContactsApp/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using ContactsApp.Classes;
+using SQLite;
+using System.Windows;
+
+namespace ContactsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReadDatabase();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+
+            ReadDatabase();
+        }
+
+        private void ReadDatabase()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                var contacts = connection.Table<Contact>().ToList();
+            }
+        }
+    }
+}
---
+++ ContactsApp/NewContactWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using ContactsApp.Classes;
+using SQLite;
+using System.Windows;
+
+namespace ContactsApp
+{
+    /// <summary>
+    /// Lógica interna para NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneTextBox.Text
+            };
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
+
+            this.Close();
+        }
+    }
+}
